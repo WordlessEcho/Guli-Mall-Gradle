f68d972b906fef135faa470f4dd6672f0ffef2e8
WordlessEcho <wordless@echo.moe>
2023-04-03 21:46:54 +0800

    Create modules as basic structure

---
+++ gulimall-storage/src/test/java/moe/echo/gulimall/storage/GulimallStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package moe.echo.gulimall.storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-user/src/test/java/moe/echo/gulimall/user/GulimallUserApplicationTests.java
@@ -0,0 +1,13 @@
+package moe.echo.gulimall.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
